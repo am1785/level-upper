e4363c5a9f4979f201fdb10ab756b57292188790
am1785 <allenmo88@gmail.com>
2024-05-07 12:05:55 -0400

    attempting cross site cookie reading

--- server/index.js
+++ server/index.js
@@ -13,6 +13,19 @@ const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+// configure app for cross-domain
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+       res.send(200);
+   } else {
+       next();
+   }
+  });
+
 // use callback function to dynamically capture all CORS routes
 
 // const whitelist = ['https://level-upper.vercel.app'];
